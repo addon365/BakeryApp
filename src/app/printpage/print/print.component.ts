60a9310aaa7892c350c41a2931ea4d3f46721957
paalamugan <39432331+paalamugan@users.noreply.github.com>
2018-05-29 12:34:34 +0530

    print page update

--- src/app/printpage/print/print.component.ts
+++ src/app/printpage/print/print.component.ts
@@ -3,13 +3,15 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { SalesOrder } from '../../models/sales-order';
 
+
+
 @Component({
   selector: 'app-print',
   templateUrl: './print.component.html',
   styleUrls: ['./print.component.css']
 })
 export class PrintComponent implements OnInit {
-
+  today: number = Date.now();
   public productForm: FormGroup;
   constructor(private _formBuilder: FormBuilder,
     private dialogRef: MatDialogRef<PrintComponent>,
@@ -21,18 +23,18 @@ export class PrintComponent implements OnInit {
 
   ngOnInit() {
     console.log(this.data.orderItems);
+  
   }
-
   @ViewChild('printsection') printsection: ElementRef;
   print(): void {
     let printContents, popupWin;
     printContents = document.getElementById('printsection').innerHTML;
     popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
-    popupWin.document.open('', 'printsection', 'height=10mm,width=72mm');
+    popupWin.document.open();
     popupWin.document.write(`
       <html>
         <head>
-        <link rel="stylesheet" href="../paper.css">
+       
           <style>
           html, body {
           
@@ -41,10 +43,10 @@ export class PrintComponent implements OnInit {
             margin: 0;
             font-size:auto;
         }
-        footer {page-break-after: always;}
+       
           @page{
-          size:75mm 20mm;
-
+          size:80mm 20mm;
+         
           }
        
         .text-center{
@@ -60,12 +62,10 @@ export class PrintComponent implements OnInit {
       }
           </style>
         </head>
-    <body onload="window.print();window.close()">${printContents}</body>
+    <body onload="window.print();window.close()"> ${printContents}</body>
       </html>`
     );
     popupWin.document.close();
 } 
-
-
-
+ 
 }
